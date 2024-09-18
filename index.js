83a2ad1a9348e214f9709190a62483dc8fc2434e
rafavd <rafarovd@gmail.com>
2024-09-17 23:12:47 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,15 @@
-const distrosea = require("./distrosea.js");
+const express = require("express");
+const { distrosea } = require("./distrosea.js");
 
-distrosea(); 
+const app = express();
+const PORT = process.env.PORT || 4000;
+
+app.get("/", (req, res) => {
+  res.send("Hello");
+});
+
+app.get("/screen", distrosea);
+
+app.listen(PORT, () => {
+  console.log(`Servidor está rodando na porta ${PORT}`);
+});
