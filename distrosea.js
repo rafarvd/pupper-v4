2e07439d0a956003a083da5c1b478e1a511a2293
rafavd <rafarovd@gmail.com>
2024-09-19 18:22:19 -0300

    Update distrosea.js

--- distrosea.js
+++ distrosea.js
@@ -25,7 +25,7 @@ const distrosea = async () => {
     ignoreAllFlags: false,
     proxy: {
       host:'5.35.70.97',
-      port:'80',
+      port:'3128',
       // username:'admin',
       // password:'w361mzdTAibA9EWF'
     }
