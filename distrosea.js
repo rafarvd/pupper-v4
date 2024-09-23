ba233adf056cb0e4138e354edfd0776eb2b5378c
rafavd <rafarovd@gmail.com>
2024-09-23 17:18:02 -0300

    Update distrosea.js

--- distrosea.js
+++ distrosea.js
@@ -27,6 +27,7 @@ const distrosea = async () => {
     turnstile: true,
     disableXvfb: false,
     ignoreAllFlags: false,
+    ignoreHTTPSErrors: true,
     // proxy: {
     //   host:'74.235.214.93',
     //   port:'3128',
