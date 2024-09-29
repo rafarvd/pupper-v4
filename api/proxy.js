8c9e357156a89c8b58ac912098d76308619c26da
Rafael Rossi <rafaelrossij@gmail.com>
2024-09-29 03:18:05 -0300

    Fix IP_PROXY fallback value in proxy.js

--- api/proxy.js
+++ api/proxy.js
@@ -9,7 +9,7 @@
 
 
 const proxy = {
-  host: process.env.IP_PROXY,
+  host: process.env.IP_PROXY || false,
   port: process.env.PORT_PROXY,
   username: process.env.USER_PROXY,
   password: process.env.PASS_PROXY,
